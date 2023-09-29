405cdf698a11459646927822379c814886c0d534
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2023-09-29 01:45:28 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,38 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
- */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+    unsigned int result = 0;
 
-	if (!b)
-		return (0);
+    if (b == NULL)
+        return 0;
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] != '0' && b[i] != '1')
+            return 0;
 
-	return (dec_val);
+        result = result << 1; // Left shift result by 1 bit
+        if (b[i] == '1')
+            result = result | 1; // Set the least significant bit to 1
+    }
+
+    return result;
+}
+
+int main(void)
+{
+    unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    return (0);
 }
